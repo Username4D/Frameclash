94848def162618b433ae2335a5880b62ac44ba41
Username4D <studio@kremzow.org>
2025-11-21 23:34:44 +0100

    improved movement rotation

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,13 @@ extends CharacterBody3D
 const speed = 120
 
 @export var bullet: PackedScene
-
+var ldirection = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up").normalized()
+	ldirection = ldirection.move_toward(direction, 2 * delta)
 	velocity = Vector3(direction.x * speed * delta, velocity.y, direction.y * -1 * speed * delta)
-	if direction != Vector2(0,0):
-		$MeshInstance3D.rotation.y = Vector2(0.0,0.0).angle_to_point(Vector2(direction.x, direction.y))
-	print(Vector2(direction.x, direction.y))
+	$MeshInstance3D.look_at($MeshInstance3D.global_position + Vector3( - ldirection.x, 0, ldirection.y))
+	
 	velocity.y -= 1 * delta
 	move_and_slide()
 
@@ -17,5 +17,6 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		var nbullet = bullet.instantiate()
 		nbullet.position = self.position
-		nbullet.rotation = $MeshInstance3D.rotation + Vector3(0, 0.5 * PI, 0)
+		
+		nbullet.rotation = $MeshInstance3D.rotation 
 		self.get_parent().add_child(nbullet)
