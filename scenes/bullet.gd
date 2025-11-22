2a7d251316c6b13a5ef40739d7e697325745b7f5
Username4D <studio@kremzow.org>
2025-11-22 22:24:30 +0100

    added enemy health tracking, bullets damaging and enemy death animation

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -5,3 +5,14 @@ extends Area3D
 
 func _physics_process(delta: float) -> void:
 	self.position += Vector3(0.0,0.0,speed).rotated(Vector3(0,1,0),self.rotation.y)
+
+func _on_body_entered(body: Node3D) -> void:
+	if enemy and body.is_in_group("player"):
+		if body.health > 0:
+			body.health -= 1
+		self.queue_free()
+	if not enemy and body.is_in_group("enemy"):
+		if body.get_node("NavigationAgent3D").health > 0:
+			body.get_node("NavigationAgent3D").health -= 1
+		self.queue_free()
+	
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,16 +1,35 @@
 extends NavigationAgent3D
 
 @export var movement_speed: float = 0.75
+@export var health =5
 
 
 func _ready() -> void:
 	self.velocity_computed.connect(Callable(_on_velocity_computed))
 	update_position()
+	$"../Label".max_value = health
 
 func set_movement_target(movement_target: Vector3):
 	self.set_target_position(movement_target)
 
 func _physics_process(delta):
+	$"../Label".value = health
+	if $"../Label".max_value == health or health < 1:
+		$"../Label".visible = false
+	else:
+		$"../Label".visible = true
+	
+	if health == 0:
+		health -= 1
+		$"../death_particles".emitting = true
+		self.get_parent().velocity = Vector3(4, 5, 0).rotated(Vector3(0,1,0), randf_range(-PI, PI))
+		self.get_parent().move_and_slide()
+	if health == -1:
+		self.get_parent().velocity.y -= 0.2
+		self.get_parent().move_and_slide()
+		if self.get_parent().is_on_floor():
+			$"../CollisionShape3D".queue_free()
+			die()
 	if self.get_parent().get_parent().get_parent().get_node("player"):
 		self.get_parent().get_node("Mesh").look_at(self.get_parent().get_parent().get_parent().get_node("player").position)
 	# Do not query when the map has never synchronized and is empty.
@@ -21,9 +40,9 @@ func _physics_process(delta):
 
 	var next_path_position: Vector3 = self.get_next_path_position()
 	var new_velocity: Vector3 = self.get_parent().global_position.direction_to(next_path_position) * movement_speed
-	if self.avoidance_enabled:
+	if self.avoidance_enabled and health > 0 :
 		self.set_velocity(new_velocity)
-	else:
+	elif health > 0:
 		_on_velocity_computed(new_velocity)
 	var own_pos = get_viewport().get_camera_3d().unproject_position(self.get_parent().position)
 	self.get_parent().get_node("Label").position = own_pos - Vector2(64,64)
@@ -37,3 +56,7 @@ func update_position():
 		set_movement_target(self.get_parent().get_parent().get_parent().get_node("player").global_position)
 
 	get_tree().create_timer(0.1).timeout.connect(update_position)
+
+func die():
+	await get_tree().create_timer(0.5).timeout
+	self.get_parent().queue_free()
