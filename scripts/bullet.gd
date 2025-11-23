49ddc16ab585c9630e6e34225591ddb7fff82940
Username4D <studio@kremzow.org>
2025-11-23 11:29:30 +0100

    added soldier enemy type

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 @export var enemy = false
 @export var speed = 0.1
+@export var damage = 4
 
 func _physics_process(delta: float) -> void:
 	self.position += Vector3(0.0,0.0,speed).rotated(Vector3(0,1,0),self.rotation.y)
@@ -11,7 +12,7 @@ func _on_body_entered(body: Node3D) -> void:
 	if enemy and body.is_in_group("player"):
 		
 		if body.health > 0:
-			body.health -= 4
+			body.health -= damage
 		self.queue_free()
 	if not enemy and body.is_in_group("enemy"):
 		if body.get_node("NavigationAgent3D").health > 0:
---
+++ scripts/soldier.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+var bullet = preload("res://scenes/bullet.tscn")
+
+func shoot():
+	
+	var shot_l = bullet.instantiate()
+	shot_l.transform = $Mesh/front_shot.global_transform
+	shot_l.enemy = true
+	shot_l.damage = 1
+	shot_l.speed = 0.05
+	self.get_parent().get_parent().add_child(shot_l)
+
+	await get_tree().create_timer(0.).timeout.connect(shoot)
+
+func _ready() -> void:
+	shoot()
