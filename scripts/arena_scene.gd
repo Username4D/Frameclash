2854d983ff5d71fbcdad69947f2d065a9282622c
Username4D <studio@kremzow.org>
2025-11-23 22:18:05 +0100

    added wavebased gameplay and integrated ui

---
+++ scripts/arena_scene.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+var wave = 0
+
+var enemies = [preload("res://scenes/tanker.tscn"), preload("res://scenes/roller.tscn"), preload("res://scenes/soldier.tscn")]
+var single_trigger = false
+func _ready() -> void:
+	start_wave()
+
+func start_wave():
+	single_trigger = false
+	wave += 1
+	gameplay_info.max_enemies = pow(2, wave + 1)
+	$Label.text = "Wave " + var_to_str(wave)
+	$wave_display.start()
+	while $wave_display.time_left != 0:
+		await get_tree().process_frame
+		$Label.position.x = -1152 + 2 * 1152 * ease($wave_display.time_left / 3, -0.2)
+	
+	for i in range(0, pow(2, wave + 1), 1):
+		var select = randi_range(0,2)
+		var obj = enemies[select].instantiate()
+		obj.position = Vector3(randi_range(-16, 16), 5, randi_range(-16, 16))
+		$enemies.add_child(obj)
+	single_trigger = true
+func _process(delta: float) -> void:
+	gameplay_info.enemies = $enemies.get_child_count()
+	if $enemies.get_child_count() == 0 and single_trigger:
+		start_wave()
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,3 +4,5 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	$live_display/Label.text = var_to_str(int(health))
+	$wave_bar/ProgressBar.max_value = gameplay_info.max_enemies
+	$wave_bar/ProgressBar.value = move_toward($wave_bar/ProgressBar.value, gameplay_info.enemies, 4 * delta)
