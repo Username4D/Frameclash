71e88695afae569682decb455e89df4c513262a8
Username4D <studio@kremzow.org>
2025-11-23 18:57:17 +0100

    added save handler

---
+++ scripts/save_handler.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var coins = 0
+var damage_upgrades = 1
+var firerate_upgrades = 1
+
+var settings = {"vsync": false, "particles": true}
+
+func s_load():
+	var file = FileAccess.open("user://einenstern.dat", FileAccess.READ)
+	if file:
+		var content = file.get_var()
+		coins = content["coins"]
+		damage_upgrades = content["damage_upgrades"]
+		firerate_upgrades = content["firerate_upgrades"]
+		settings = content["settings"]
+
+func s_save():
+	var content = {"coins": coins, "damage_upgrades": damage_upgrades, "firerate_upgrades": firerate_upgrades, "settings": settings}
+	var file = FileAccess.open("user://einenstern.dat", FileAccess.WRITE)
+	file.store_var(content)
